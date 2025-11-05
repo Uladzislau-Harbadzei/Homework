a4205e358e2ef9369e9c2091812c2357737a330c
Uladzislau-Harbadzei <Uladzislau_Harbadzei@epam.com>
2025-11-05 14:25:18 +0300

    add favicon

--- src/scripts/greeting.js
+++ src/scripts/greeting.js
@@ -3,4 +3,4 @@ const url = window.location.href;
 const splitter = '=';
 const index = url.indexOf(splitter) + 1;
 const username = url.slice(index);
-greeting.textContent = `Hello my friend ${username}`;
+greeting.textContent = `Hello my friend \n ${username}`;
