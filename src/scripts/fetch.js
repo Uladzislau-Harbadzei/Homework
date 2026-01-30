bbf45c9130ed87e217c6dd2f7c53b0c43ce134ca
Uladzislau-Harbadzei <Uladzislau_Harbadzei@epam.com>
2026-01-30 11:20:32 +0300

    add fetch

---
+++ src/scripts/fetch.js
@@ -0,0 +1,12 @@
+document.getElementById('usersbuton').addEventListener('click', function () {
+  fetch('https://dummyjson.com/users')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+});
